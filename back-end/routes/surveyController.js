419fe1d977cfce8ff0dce2fc9fa483b142dd5bf4
Gaurav Savner <gaurav.savner@sjsu.edu>
2018-11-28 16:32:57 -0800

    fetch survey backend complete

--- back-end/routes/surveyController.js
+++ back-end/routes/surveyController.js
@@ -22,18 +22,54 @@ exports.fetchMySurveys = function fetchMySurveys(req, res) {
                 if (err) throw err;
                 console.log(rows);
                 // send data to frontend
+                res.json(rows);
             })
         })
     }
-    res.send({ "Reply": "done" });
 }
 
 // FETCH SURVEY SHARED WITH LOGGED IN USER - GET: '/fetchSharedWithMe'
 exports.fetchSharedWithMe = function fetchSharedWithMe(req, res) {
     console.log(`Inside fetchSharedWithMe`);
+
+    // getting the user email from req.body.email, query invites table
+    var sql = 'SELECT sname FROM survey as s WHERE s.id in (SELECT s_id FROM INVITES WHERE invite_email = ' + mysql.pool.escape(req.body.email) + ')';
+
+    if (DATABASE_POOL) {
+
+        mysql.pool.getConnection(function (err, connection) {
+            if (err)
+                return res.status(400).send(responseJSON("SERVER_someError"));
+
+            // if you got a connection...
+            connection.query(sql, function (err, rows, fields) {
+                if (err) throw err;
+                // send data to frontend
+                res.json(rows);
+            })
+        })
+    }
 }
 
 // FETCH VOLUNTARY SURVEY - GET: '/fetchSharedWithMe'
 exports.fetchVolunteerSurvey = function fetchVolunteerSurvey(req, res) {
     console.log(`Inside fetchVolunteerSurvey`);
+
+    var sql = 'SELECT sname FROM survey where stype = "VOLUNTARY"';
+
+    if (DATABASE_POOL) {
+
+        mysql.pool.getConnection(function (err, connection) {
+            if (err)
+                return res.status(400).send(responseJSON("SERVER_someError"));
+
+            // if you got a connection...
+            connection.query(sql, function (err, rows, fields) {
+                if (err) throw err;
+                console.log(rows);
+                // send data to frontend
+                res.json(rows);
+            })
+        })
+    }
 }
