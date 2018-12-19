a7026f0c884ca8cd878964899bd88ba9a9056187
Gaurav Savner <gaurav@Gauravs-MacBook-Pro.local>
2018-12-18 19:12:40 -0800

    cleaned working repository

--- front-end/public/Sourcetree.app/Contents/Resources/araxishgmerge-w.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# we need to make sure our path includes all the likely places to find Araxis
-# command-line tools
-export PATH=~/bin:/usr/local/bin:/usr/bin:/bin:/Applications/Araxis\ Merge.app/Contents/Utilities:$PATH
-
-araxishgmerge "$@"
-
--- front-end/public/Sourcetree.app/Contents/Resources/git-merge-one-file.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# git merge-index needs a single program name, so we encapsulate 'git merge-one-file' in a script
-git merge-one-file "$@"
-
-
